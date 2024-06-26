716a1ea457d224c2feee9176444e8ccee3a259b6
Tima <takumishiawase.it@gmail.com>
2024-06-26 06:22:08 +0500

    updateNew

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/genecats" element={<Home />} />
-        <Route path='/welcome' element={ <Welcome />} />
+        <Route path='/genecats/welcome' element={ <Welcome />} />
       </Routes>
     </Router>
   );
